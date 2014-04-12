4e1b6b13b6ac0b8bb352ebff6c8f629ef6149c26
Robin Lee <robincrlee@gmail.com>
2014-04-12 15:44:08 -0700

    Added Home and About pages

---
+++ pinteresting/app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController # define the file
+  def home 
+  	# this has something to do with the homepage. 
+  	# its empty becuase we're not interacting with DB yet. 
+  end
+
+  def about # we have two pages
+  end
+end
